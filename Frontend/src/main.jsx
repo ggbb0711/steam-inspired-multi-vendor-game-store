6b0e62e869a2941578f0acc50d04e838a64bd4a3
ggbb0711 <ggbotcute07@gmail.com>
2023-10-20 12:53:30 +0700

    Disable react tool for production

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {UserContext} from './util/components/Context/userContex.jsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.ENV==='PRODUCTION') disableReactDevTools()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserContext>
